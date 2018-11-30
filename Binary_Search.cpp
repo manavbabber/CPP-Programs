6f9f6a9af5055b0642723217c6e926af484b5406
Manavbabber <manavfazilka@gmail.com>
2018-11-30 13:03:58 +0000

    Binary search

---
+++ Binary_Search.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{ int arr[20],n,search,first,last,middle;
+ 
+ cout << "Enter the number of Elements in an array:"<<endl;
+ cin>>n;
+cout<<"Enter the elements of an array:"<<endl;
+for(int i=0;i<n;i++)
+{
+cin>>arr[i];
+}
+first=0;
+last=n;
+middle=(first+last)/2;
+cout<<"Enter the number u want to search:"<<endl;
+cin>>search;
+while(first<=last)
+{
+if(arr[middle]< search)
+{
+first = middle +1;
+}
+else if(arr[middle]==search)
+{
+cout<<"Position of"<<search<<"is:"<<middle+1<<endl;
+break;
+}
+else{
+last=middle-1;
+}
+middle=(first+last)/2;
+}
+if(first>last)
+{
+cout<<"OOPS!!!! Number not found"<<endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
